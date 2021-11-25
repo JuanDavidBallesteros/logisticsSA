9096a542b46f745beb6ff0eb28eb31bbeeb3a284
samuel guerrero <samuel040902@hotmail.com>
2021-11-24 23:20:07 -0500

    Add files via upload

---
+++ test/model/DFSTest.java
@@ -0,0 +1,60 @@
+package model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class DFSTest {
+
+    public void setupScenery(){
+    Graph graph = new Graph();
+
+    Vertex v1= new Vertex('A');
+    Vertex v2= new Vertex('B');
+    Vertex v3= new Vertex('C');
+    Vertex v4= new Vertex('D');
+    Vertex v5= new Vertex('E');
+    Vertex v6= new Vertex('F');
+
+
+        graph.addNode(v1);
+        graph.addNode(v2);
+        graph.addNode(v3);
+        graph.addNode(v4);
+        graph.addNode(v5);
+        graph.addNode(v6);
+
+        graph.addEdgeBFS(v1, v2, 2);
+        graph.addEdgeBFS(v1, v3, 5);
+        graph.addEdgeBFS(v1, v4, 6);
+        graph.addEdgeBFS(v3, v5, 1);
+        graph.addEdgeBFS(v3, v6, 2);
+
+    
+    }
+
+    @Test
+    public void test() {
+
+        DFS dfs = new DFS();
+
+
+        assertEquals("The method should return output based on DFS",
+                "A B C D E F ", dfs.getReadable(dfs.performDFS(graph, v1)));
+        assertEquals("The method should return output based on DFS",
+                "C E F ", dfs.getReadable(dfs.performDFS(graph, v3)));
+        assertEquals("The method should return output based on DFS",
+                "B ", dfs.getReadable(dfs.performDFS(graph, v2)));
+        assertEquals("The method should return output based on DFS",
+                "D ", dfs.getReadable(dfs.performDFS(graph, v4)));
+        assertEquals("The method should return output based on DFS",
+                "E ", dfs.getReadable(dfs.performDFS(graph, v5)));
+        assertEquals("The method should return output based on DFS",
+                "F ", dfs.getReadable(dfs.performDFS(graph, v6)));
+
+    }
+
+}
